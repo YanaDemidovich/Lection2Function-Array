bc37318fceeba49d264b16d20cfd860098b3bdaa
Yana Demidovich <demyanvo1990@gmail.com>
2022-07-12 22:42:21 +0300

    Create excercise3

---
+++ Excercise3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Метот и генератор случайных чисел
+
+void FillArray(int[collection])
+{
+    int length = collection.Lenght;
+    int index = 0;
+    while(index < lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position <count)
+    {
+        Console.WhriteLine(col[position]);
+        position++;
+    }
+}
+
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
